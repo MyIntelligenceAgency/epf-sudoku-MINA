94dc9b5a20aaaee9c28695c1d31ffff78eb8751a
Nicolas Vauvillier <nicolas.vauvillier@epfedu.fr>
2019-10-24 23:57:50 +0200

    update

--- Sudoku/SolverDLX/SolverDlx.cs
+++ Sudoku/SolverDLX/SolverDlx.cs
@@ -14,7 +14,7 @@ namespace SolverDLX
             int[,] mySudoku = new int[9, 9];
             for (int i = 0; i < 9; i++) {
                 for (int j = 0; j < 9; j++) {
-                    mySudoku[i, j] = mySudoku[i, j];
+                    mySudoku[i, j] = mySudoku[i, s.Cells[j]];
                 }
               }
 
